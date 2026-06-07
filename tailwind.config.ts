07bafd5cf56ab879d2a8dd3fb8d0a3e57122fb32
川村健介 <kensuke_kawamura@kawamurakensukenoMacBook-Air.local>
2026-06-07 15:23:19 +0900

    add password login

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./app/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}", "./types/**/*.{ts,tsx}"],
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
+    "./types/**/*.{ts,tsx}",
+  ],
   theme: {
     extend: {
       boxShadow: {
